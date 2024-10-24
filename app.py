1b2fcd3d7f0fbd635fb64469d5aa5655307b97b1
ElinaSa <156064412+ElinaSa@users.noreply.github.com>
2024-10-24 11:57:40 +0300

    tallennetaan muutokset ennen uutta korjattua dev-app2 -haaraa

--- app.py
+++ app.py
@@ -7,17 +7,24 @@
 # MODUULIT
 # --------
 
-import sound2 # Äänimerkit ja äänitiedostot
-import video2 # Videomoduuli
+from avtools import sound2 # Äänimerkit ja äänitiedostot
+from avtools import video2 # Videomoduuli
+import identityCheck2
 
 # ASETUKSET
 # ---------
 
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
-# Käynnistetään videokuva ja ilmoitetaan sen käynnistymisestä äänimerkillä
-sound2.parametricBeep(400,330)
-sound2.playWav('Alkaa.WAV')
+userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
+ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
+if ssnToCheck.isValidSsn() == True:
+    dateOfBirth = ssnToCheck.getDateOfBirth()
+    ssnToCheck.getGender()
+    age = ssnToCheck.calculateAge()
+    print('Syntymäaika:', ssnToCheck.dateOfBirth)
+    print('Ikä:', age)
+    print('Sukupuoli:', ssnToCheck.gender)
 
 # TESTIT KOODAUKSEN AIKANA
 # ========================
