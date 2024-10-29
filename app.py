920cb5d886a498fda5bb67f0f5d13b975a588938
ElinaSa <156064412+ElinaSa@users.noreply.github.com>
2024-10-29 09:32:26 +0200

    jotain muutoksia

--- app.py
+++ app.py
@@ -16,18 +16,77 @@ import identityCheck2
 
 kameraIndeksi: int = 1 # Ensimmäinen kamera on aina 0
 
-userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
-ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
-if ssnToCheck.isValidSsn() == True:
-    dateOfBirth = ssnToCheck.getDateOfBirth()
-    ssnToCheck.getGender()
-    age = ssnToCheck.calculateAge()
-    print('Syntymäaika:', ssnToCheck.dateOfBirth)
-    print('Ikä:', age)
-    print('Sukupuoli:', ssnToCheck.gender)
+# FUNKTIOT
+# --------
 
-# TESTIT KOODAUKSEN AIKANA
-# ========================
+def askName(question: str) -> str:
+    """Promts user to answer a question and convert the answer to title and removes white space
+
+    Args:
+        question (str): Prompt to user
+
+    Returns:
+        str: modifies answer
+    """
+    name = ''
+    while name == '':
+        question = question + ': '
+        name = input(question).strip()
+    name = name.title()
+    return name
 
 if __name__ == "__main__":
-    pass
+    
+    # PÄÄOHJELMAN IKUIN SILMUKKA
+    #===========================
+
+    while True:
+
+        # Alustetaan muuttujat tyhjiksi
+        userGivenSsn = ''
+        userGivenLastname = ''
+        userGivenFirstname = ''
+
+        # Kysytään asiakkaan henkilötunnus ja muutetaan kirjaimet isoiksi
+        userGivenSsn = input('Syötä asiakkaan henkilötunnus: ')
+        userGivenSsn = userGivenSsn.upper() # Varmistetaan, että tarkiste on isolla
+
+
+
+
+
+        # Luodaanm käytetystä henkilötunnuksesta NationalSSN-objekti
+        ssnToCheck = identityCheck2.NationalSSN(userGivenSsn)
+
+        # Tarkistetaan onko henkilötunnus oikein muodostettu
+        if ssnToCheck.isValidSsn() == True:
+
+            # Virheenkäsittely, mahdollisen vuosisatakoodivirheen varalta
+            try:
+                ssnToCheck.getDateOfBirth() # Asetetaan syntymäaikaominaisuus
+                ssnToCheck.getGender() # Asetetaan sukupuoliominaisuus
+                age = ssnToCheck.calculateAge() # Lasketaan ikä tänään
+
+                # Kysytään loput tiedot, jos ei virheitä
+                userGivenLastname = askName('Asiakkaan sukunimi')
+                userGivenFirstname = askName('Asiakkaan etunimi')
+
+                # Tulostetaan tiedot ruudulle
+                print('Asiakas:', userGivenLastname, userGivenFirstname)
+                print('Syntymäaika:', ssnToCheck.dateOfBirth)
+                print('Ikä:', age)
+                print('Sukupuoli:', ssnToCheck.gender)
+
+            # Virhetilanteessa näytetään virheilmoitus
+            except Exception as e:
+                print('Syöttämässäsi sosiaaliturvatunnuksessa oli virhe', e)
+
+        else:
+            print('Henkilötunnuksessa virhe, syötä tunnus uudelleen')
+
+        # Kysytään halutaanko poistua ohjelmasta
+        wantToAbort = input('Haluatko päättää ohjelman k/E: ')
+        # Muutetaan vastaus isoiksi kirjaimiksi ja tarkistetaan onko vastaus K
+        if wantToAbort.upper() == 'K':
+            break # Poistutaan ikuisesta silmukasta
+
